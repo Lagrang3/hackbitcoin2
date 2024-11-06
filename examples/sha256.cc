145cc28a57864821b0b2c706fd9687cb6c435530
Lagrang3 <lagrang3@protonmail.com>
2024-11-06 14:31:34 +0100

    example sha256
    Signed-off-by: Lagrang3 <lagrang3@protonmail.com>

--- examples/sha256.cc
+++ examples/sha256.cc
@@ -1,14 +1,29 @@
 #include <sodium.h>
 
+#include <array>
 #include <iostream>
+#include <string>
+#include <vector>
 
 int main(void) {
-	std::cout << "hello world" << std::endl;
 	if (sodium_init() < 0) {
 		/* panic! the library couldn't be initialized; it is not safe to
 		 * use */
 		exit(1);
 	}
-	std::cout << "started sodium" << std::endl;
+	std::string s;
+	// read a text from stdin
+	std::cin >> s;
+	std::vector<unsigned char> s_data(s.begin(), s.end());
+	
+	// hash it
+	std::array<unsigned char, crypto_hash_sha256_BYTES> s_hash;
+	crypto_hash_sha256(s_hash.data(), s_data.data(), s.size());
+	
+	// print it in hex
+	std::array<char, 2 * crypto_hash_sha256_BYTES + 1> s_hash_hex;
+	sodium_bin2hex(s_hash_hex.data(), s_hash_hex.size(), s_hash.data(),
+		       s_hash.size());
+	std::cout << s_hash_hex.data() << std::endl;
 	return 0;
 }
